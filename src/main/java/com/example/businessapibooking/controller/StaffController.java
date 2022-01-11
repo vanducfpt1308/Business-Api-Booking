a8288ac13bad03e64194ce6f140736323b1d723c
vanducfpt1308 <59786168+vanducfpt1308@users.noreply.github.com>
2022-01-11 21:39:29 +0700

    register Staff

--- src/main/java/com/example/businessapibooking/controller/StaffController.java
+++ src/main/java/com/example/businessapibooking/controller/StaffController.java
@@ -1,7 +1,14 @@
 package com.example.businessapibooking.controller;
 
+import com.example.businessapibooking.dto.RegisterStaffRequest;
+import com.example.businessapibooking.entity.Role;
 import com.example.businessapibooking.entity.Staff;
+import com.example.businessapibooking.entity.Users;
+import com.example.businessapibooking.repository.RoleRepo;
+import com.example.businessapibooking.repository.StaffRepository;
+import com.example.businessapibooking.repository.UserRepo;
 import com.example.businessapibooking.service.StaffService;
+import com.example.businessapibooking.utils.HashUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +27,15 @@ public class StaffController {
     public static final Logger LOGGER = LoggerFactory.getLogger(StaffController.class);
     @Autowired
     StaffService service;
+
+    @Autowired
+    private RoleRepo roleRepo;
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    StaffRepository staffRepository;
 
     @RequestMapping(value = {"/getAll"}, method = RequestMethod.GET)
     public ResponseEntity<?> getAll() {
@@ -74,5 +90,39 @@ public class StaffController {
             LOGGER.info("getAll :" + startTime);
         }
         return new ResponseEntity<>(staff, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = {"/registerDoctor"}, method = RequestMethod.POST)
+    public ResponseEntity<?> registerDoctor(@RequestBody RegisterStaffRequest registerRequest) {
+        long startTime = System.currentTimeMillis();
+        Staff staff = new Staff();
+        Role role = roleRepo.findByRole("DOCTOR");
+        Users user = new Users();
+        try {
+            if (registerRequest != null) {
+                user.setPassword(HashUtil.hash(registerRequest.getPassword()));
+                user.setUsername(registerRequest.getUsername());
+                user.setRole(role);
+                userRepo.save(user);
+
+                staff.setEmail(registerRequest.getEmail());
+                staff.setUser(user);
+                staff.setFullName(registerRequest.getFullName());
+                staff.setPhoneNumber(registerRequest.getPhone());
+                staff.setGender(registerRequest.getGender());
+                staff.setDateStartWork(registerRequest.getDateStartWork());
+                staff.setAcademicLevel(registerRequest.getAcademicLevel());
+                staff.setDescription(registerRequest.getDescription());
+                staff.setPhoto(registerRequest.getPhoto());
+
+                staffRepository.save(staff);
+            }
+
+        } catch (Exception ex) {
+            throw ex;
+        } finally {
+            LOGGER.info("register :" + startTime);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
