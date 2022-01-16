92b3a232d2fba7e6ab8ebf67f6d2d89619969c45
Thành Long <59786092+longttph12421@users.noreply.github.com>
2022-01-16 22:41:45 +0700

    new

--- src/main/java/com/example/businessapibooking/repository/BookingDetailRepo.java
+++ src/main/java/com/example/businessapibooking/repository/BookingDetailRepo.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -16,6 +17,12 @@ public interface BookingDetailRepo extends JpaRepository<BookingDetail, Integer>
     @Query("SELECT b FROM BookingDetail b WHERE b.booking.customer.id = :id and b.status = :stt")
     List<BookingDetail> findByCustomerAndStatus(Integer id, Integer stt);
 
+    @Query("SELECT b FROM BookingDetail b WHERE b.dateBooking = :date and b.status = :stt")
+    List<BookingDetail> findByDateBookingAndStatus(Date date, Integer stt);
+
+    @Query("SELECT b FROM BookingDetail b WHERE b.staff.id = :id and b.status = :stt")
+    List<BookingDetail> findByDoctor(Integer id, Integer stt);
+    
     @Query("SELECT COUNT(bk.id) from BookingDetail bk where MONTH(bk.dateBooking) = :month\n" +
             "and YEAR(bk.dateBooking)=2021\n" +
             "and bk.status = 2")
